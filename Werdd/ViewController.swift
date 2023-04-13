15c2b8efa8f4476e0cdabb0cc54a98585d16d77c
Ronin619 <eytam619@gmail.com>
2023-04-12 23:20:57 -0400

    feat: implement word box view

--- Werdd/ViewController.swift
+++ Werdd/ViewController.swift
@@ -9,10 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let wordView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .cyan
+        view.layer.cornerRadius = 20
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
+
+        view.backgroundColor = .lightGray
+
+        view.addSubview(wordView)
+        
+        NSLayoutConstraint.activate([
+            wordView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            wordView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            wordView.heightAnchor.constraint(equalToConstant: 300),
+            wordView.widthAnchor.constraint(equalToConstant: 350)
+        ])
     }
 
 
