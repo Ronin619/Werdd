cea3e5d78ee39315cb373cf7b4a6d9b477aa5eeb
Ronin619 <eytam619@gmail.com>
2023-04-12 23:49:33 -0400

    feat: implement header label

--- Werdd/ViewController.swift
+++ Werdd/ViewController.swift
@@ -8,8 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
+    let headerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Werdd."
+        return label
+    }()
+
     let wordView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -22,14 +28,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .lightGray
-
+        
+        view.addSubview(headerLabel)
         view.addSubview(wordView)
         
         NSLayoutConstraint.activate([
             wordView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             wordView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             wordView.heightAnchor.constraint(equalToConstant: 300),
-            wordView.widthAnchor.constraint(equalToConstant: 350)
+            wordView.widthAnchor.constraint(equalToConstant: 350),
+        ])
+        
+        NSLayoutConstraint.activate([
+            headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            headerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            headerLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor),
         ])
     }
 
