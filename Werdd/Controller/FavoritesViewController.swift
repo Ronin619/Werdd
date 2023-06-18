38c1562d59e51b5969aac63cb28799bf7245c99b
Ronin619 <eytam619@gmail.com>
2023-06-18 13:36:27 -0400

    feat: initialize scrollview in favorites view controller

--- Werdd/Controller/FavoritesViewController.swift
+++ Werdd/Controller/FavoritesViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class FavoritesViewController: UIViewController {
-
+    
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    init() {
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        return nil
+    }
+    
     override func viewDidLoad() {
+        view.backgroundColor = UIColor(named: "lightGrey")
         super.viewDidLoad()
-
     }
     
 }
