e136fe726bf3405641a4a62b156e64de2f3fa3d9
Ronin619 <eytam619@gmail.com>
2023-06-18 13:30:03 -0400

    Feat: implement Favorites view controller

---
+++ Werdd/Controller/FavoritesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesViewController.swift
+//  Werdd
+//
+//  Created by Eric Tam on 6/18/23.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
